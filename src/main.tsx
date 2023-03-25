0694d66f597954d11ba97744d034b5c92a2d484b
Daichi Kimura <dkimura@bdd.xyz>
2023-03-25 23:15:13 +0900

    Update Auth0Provider properties to use Vite environment variables

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN ?? ''}
-      clientId={process.env.AUTH0_CLIENT_ID ?? ''}
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
