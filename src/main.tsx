02da88e7e81b10fc9596aea6fc53d9f087ce05a4
Daichi Kimura <dkimura@bdd.xyz>
2023-03-25 22:53:38 +0900

    Add @auth0/auth0-react and @generouted/react-router dependencies and configure Auth0Provider in main.tsx. Also, add Home and SignIn pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { Routes } from 'generouted/react-router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.AUTH0_DOMAIN ?? ''}
+      clientId={process.env.AUTH0_CLIENT_ID ?? ''}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <Routes />
+    </Auth0Provider>
   </React.StrictMode>
 )
