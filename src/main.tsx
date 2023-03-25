618682adccbd66b14c946338ab1cd2ae4490737b
Daichi Kimura <dkimura@bdd.xyz>
2023-03-25 23:15:25 +0900

    Delete index.css import in main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { Routes } from 'generouted/react-router'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
