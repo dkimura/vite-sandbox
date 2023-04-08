87d738e1554ed6aa1a5977adf7ff296e1a1cc1b8
Daichi Kimura <dkimura@bdd.xyz>
2023-04-09 05:44:52 +0900

    feat: Migrate generouted to v1.12.0

--- apps/vite/src/main.tsx
+++ apps/vite/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
-import { Routes } from 'generouted/react-router'
+import { Routes } from '@generouted/react-router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
