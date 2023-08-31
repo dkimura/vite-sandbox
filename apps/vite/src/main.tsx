9d3570adb47f93fee063ba63e6900dfa8b3b47f2
Daichi Kimura <dkimura@bdd.xyz>
2023-09-01 08:05:48 +0900

    feat: Add Button component to login page

--- apps/vite/src/main.tsx
+++ apps/vite/src/main.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { Routes } from '@generouted/react-router'
+import { Provider } from '@dkimura/ui'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <Routes />
-    </Auth0Provider>
+    <Provider>
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <Routes />
+      </Auth0Provider>
+    </Provider>
   </React.StrictMode>
 )
--- apps/vite/src/pages/index.tsx
+++ apps/vite/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@dkimura/ui'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const Home = () => {
@@ -19,7 +20,7 @@ const Home = () => {
       </div>
     )
   } else {
-    return <button onClick={() => loginWithRedirect()}>Log in</button>
+    return <Button onClick={() => loginWithRedirect()}>Log in</Button>
   }
 }
 
