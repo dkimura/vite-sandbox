1ff8e6981f934453c51fc8b5935e91b74bdba27f
Daichi Kimura <dkimura@bdd.xyz>
2023-09-25 15:19:13 +0900

    chore: update Button.stories.tsx

--- packages/ui/src/components/Button.stories.tsx
+++ packages/ui/src/components/Button.stories.tsx
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { Button } from './Button'
 
-//👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof Button> = {
   component: Button,
   args: {
@@ -13,8 +12,4 @@ const meta: Meta<typeof Button> = {
 export default meta
 type Story = StoryObj<typeof Button>
 
-export const FirstStory: Story = {
-  args: {
-    //👇 The args you need here will depend on your component
-  },
-}
+export const Base: Story = {}
